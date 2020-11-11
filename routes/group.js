02b0aea60e79f7d96d1f3bb5ba4e215b901255a2
tjdrnr0557 <tjdrnr05571@naver.com>
2020-11-11 23:26:04 +0900

    그룹 규칙 추가

--- routes/group.js
+++ routes/group.js
@@ -10,6 +10,7 @@ const router = express.Router();
 router.post("/", async (req, res, next) => {
   try {
     console.log("@@@@@@@@" + JSON.stringify(req.body)); //@@@@ 로깅은 이렇게 하시면 돼요 콘솔창에 뜹니다.
+
     const newGroup = await db.Group.create({
       name: req.body.name,
       groupCategory: req.body.groupCategory,
@@ -25,6 +26,18 @@ router.post("/", async (req, res, next) => {
       friday: req.body.friday,
       saturday: req.body.saturday,
       sunday: req.body.sunday,
+    });
+
+    const authRule = req.body.authRule;
+    const dbAuthRule = authRule.map((a) => {
+      if (a) return a;
+    });
+
+    dbAuthRule.map((d) => {
+      db.Group_Notice.create({
+        content: d,
+        groupId: newGroup.id,
+      });
     });
 
     await db.Group_User.create({
