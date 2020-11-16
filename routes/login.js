41277b75c0d646d018f3a1cc8afc0915a5438236
ydab1996 <ydab1996@naver.com>
2020-11-16 17:08:30 +0000

    normal login

--- routes/login.js
+++ routes/login.js
@@ -85,7 +85,70 @@ router.post('/kakao', async (req, res, next) => {
 });
 
 
-router.get("/kakao", jwtMiddleware, async (req, res) => {
+router.post('/', async (req, res, next) => {
+
+    //시퀄라이즈 때문에 대충 이런식으로 한다고만 올려둔거에요 알아서 로직 바꿔서 로그인 짜시면 될거같습니다.이부분은 다 지우고 하셔도 상관없어요.
+
+
+    // console.log("come!");
+    try {
+
+                    const exUser = await db.User.findOne({
+                        where: {
+                            userId: req.body.userId,
+                            password: req.body.password,
+                        }
+                    });
+                    if (!exUser) {
+                        res.json({
+                            isSuccess: false,
+                            code: 404,
+                            message: "아이디와 패스워드를 확인해주세요.",
+                        });
+                        //connection.r
+                    }
+                    else {
+                        let token = await jwt.sign(
+                            {
+                                userId: exUser.userId,
+                            }, // 토큰의 내용(payload)
+                            secret_config.jwtsecret, // 비밀 키
+                            {
+                                expiresIn: "365d",
+                                subject: "userInfo",
+                            } // 유효 시간은 365일
+                        );
+                        //console.log(token);
+                        res.json({
+                            jwt: token,
+                            isSuccess: true,
+                            code: 200,
+                            message: "로그인 성공",
+                        });
+                        //connection.release();
+                    }
+                } else {
+                    return res.json({
+                        code : response.statusCode,
+                        isSuccess : false,
+                        error_message : response.body
+                    });
+                }
+            }
+        );
+
+
+        //return res.status(200).json(newUser);
+    } catch (e) {
+        console.error(e);
+        return next(e);
+    }
+
+
+});
+
+
+router.get("/", jwtMiddleware, async (req, res) => {
     const {userId} = req.verifiedToken;
     try {
         const User = await db.User.findOne({
